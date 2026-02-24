88f4beab57216b68e3c1e2329d123926b0e37895
yazdancode <yshabanei@gmail.com>
2026-02-24 18:20:49 +0330

    reset-password

---
+++ system/Config/Env.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace System\Config;
+
+class Env
+{
+    public static function load($path)
+    {
+        if (!file_exists($path)) {
+            return;
+        }
+
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($key, $value) = explode('=', $line, 2);
+
+            $key = trim($key);
+            $value = trim($value);
+
+            $_ENV[$key] = $value;
+            putenv("$key=$value");
+        }
+    }
+}
