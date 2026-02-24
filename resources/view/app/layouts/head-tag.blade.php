47e91070d72736c5a715611e028b1a10acaf14d1
yazdancode <yshabanei@gmail.com>
2026-02-24 22:21:28 +0330

    app

--- resources/view/app/layouts/head-tag.blade.php
+++ resources/view/app/layouts/head-tag.blade.php
@@ -1,24 +1,24 @@
 <title>Royal Estate</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
 
-    <link rel="stylesheet" href="<?=asset('css/open-iconic-bootstrap.min.css')?>">
-    <link rel="stylesheet" href="<?= asset('css/animate.css') ?>">
+<link rel="stylesheet" href="<?=asset('css/open-iconic-bootstrap.min.css')?>">
+<link rel="stylesheet" href="<?= asset('css/animate.css') ?>">
 
-    <link rel="stylesheet" href="<?= asset('css/owl.carousel.min.css')?>">
-    <link rel="stylesheet" href="<?= asset('css/owl.theme.default.min.css')?>">
-    <link rel="stylesheet" href="<?= asset('css/magnific-popup.css')?>">
+<link rel="stylesheet" href="<?= asset('css/owl.carousel.min.css')?>">
+<link rel="stylesheet" href="<?= asset('css/owl.theme.default.min.css')?>">
+<link rel="stylesheet" href="<?= asset('css/magnific-popup.css')?>">
 
-    <link rel="stylesheet" href="<?= asset('css/aos.css')?>">
+<link rel="stylesheet" href="<?= asset('css/aos.css')?>">
 
-    <link rel="stylesheet" href="<?= asset('css/ionicons.min.css')?>">
+<link rel="stylesheet" href="<?= asset('css/ionicons.min.css')?>">
 
-    <link rel="stylesheet" href="<?= asset('css/bootstrap-datepicker.css')?>">
-    <link rel="stylesheet" href="<?= asset('css/jquery.timepicker.css')?>">
+<link rel="stylesheet" href="<?= asset('css/bootstrap-datepicker.css')?>">
+<link rel="stylesheet" href="<?= asset('css/jquery.timepicker.css')?>">
 
 
-    <link rel="stylesheet" href="<?= asset('css/flaticon.css')?>">
-    <link rel="stylesheet" href="<?= asset('css/icomoon.css')?>">
-    <link rel="stylesheet" href="<?= asset('css/style.css')?>">
+<link rel="stylesheet" href="<?= asset('css/flaticon.css')?>">
+<link rel="stylesheet" href="<?= asset('css/icomoon.css')?>">
+<link rel="stylesheet" href="<?= asset('css/style.css')?>">
--- resources/view/app/layouts/scripts.blade.php
+++ resources/view/app/layouts/scripts.blade.php
@@ -13,6 +13,5 @@
 <script src="<?= asset('js/jquery.timepicker.min.js') ?>"></script>
 <script src="<?= asset('js/scrollax.min.js') ?>"></script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-
 <script src="<?= asset('js/google-map.js') ?>"></script>
 <script src="<?= asset('js/main.js') ?>"></script>
