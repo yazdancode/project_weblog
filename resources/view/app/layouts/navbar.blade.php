6cd81c2d1909b09f9dd6194262bd92eefe54dcea
yazdancode <yshabanei@gmail.com>
2026-02-24 21:27:31 +0330

    app

---
+++ resources/view/app/layouts/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar" style="direction: rtl;">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">Royal<span>estate</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> منو
+      </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active"><a href="index.html" class="nav-link">خانه</a></li>
+                <li class="nav-item"><a href="property.html" class="nav-link">آگهی ها</a></li>
+                <li class="nav-item"><a href="about.html" class="nav-link">درباره ما</a></li>
+                <li class="nav-item"><a href="blog.html" class="nav-link">بلاگ</a></li>
+                <li class="nav-item cta"><a href="contact.html" class="nav-link ml-lg-1 mr-lg-5"><span class="icon-user m-2"></span>ورود</a></li>
+                <li class="nav-item cta cta-colored"><a href="contact.html" class="nav-link"><span class="icon-pencil m-2"></span>ثبت نام</a></li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- END nav -->
