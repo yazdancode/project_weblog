d3a690d5283b935bf2891e7ff5ea40c6b6bc4bb4
yazdancode <yshabanei@gmail.com>
2026-02-24 22:16:16 +0330

    app

--- resources/view/app/layouts/scripts.blade.php
+++ resources/view/app/layouts/scripts.blade.php
@@ -1,5 +1,5 @@
-<script src="js/jquery.min.js"></script>
-<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="<?= asset('js/jquery.min.js')?>"></script>
+<script src="<?= asset('js/jquery-migrate-3.0.1.min.js')?>"></script>
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/jquery.easing.1.3.js"></script>
