b3f33464a1ceb1ba161aaecc8330778051be69b8
yazdancode <yshabanei@gmail.com>
2026-02-24 20:12:13 +0330

    reset-password

--- routes/web.php
+++ routes/web.php
@@ -73,5 +73,6 @@ Route::get('/forgot', 'Auth\ForgotController@view', 'auth.forgot.password');
 Route::post('/forgot', 'Auth\ForgotController@forgot', 'auth.forgot');
 Route::get('/reset-password/{token}', 'Auth\ResetPasswordController@view', 'auth.reset-password.view');
 Route::post('/reset-password/{token}', 'Auth\ResetPasswordController@resetPassword', 'auth.reset-password');
+Route::get('/logout', 'Auth\LogoutController@logout', 'auth.logout');
 
 
